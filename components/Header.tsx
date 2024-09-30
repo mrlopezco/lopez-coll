b38a7de481f5feb06f5622de1cb6df691ab20ba5
Matias Jurado <juradomatias15@gmail.com>
2024-09-30 18:09:29 -0300

    Init project based on tailwind-nextjs-starter-blog

---
+++ components/Header.tsx
@@ -0,0 +1,66 @@
+'use client'
+
+import clsx from 'clsx'
+import { usePathname } from 'next/navigation'
+import siteMetadata from '@/data/siteMetadata'
+import headerNavLinks from '@/data/headerNavLinks'
+import Logo from '@/data/logo.svg'
+import Link from './Link'
+import MobileNav from './MobileNav'
+import ThemeSwitch from './ThemeSwitch'
+import SearchButton from './SearchButton'
+
+const Header = () => {
+  const pathname = usePathname()
+
+  let headerClass =
+    'flex items-center w-full bg-white/75 dark:bg-dark/75 justify-between py-4 upports-backdrop-blur backdrop-blur'
+  if (siteMetadata.stickyNav) {
+    headerClass += ' sticky top-0 z-50'
+  }
+
+  return (
+    <header className={headerClass}>
+      <Link href="/" aria-label={siteMetadata.headerTitle}>
+        <div className="flex items-center justify-between">
+          <div className="mr-3">
+            <Logo />
+          </div>
+          {typeof siteMetadata.headerTitle === 'string' ? (
+            <div className="group hidden text-xl font-semibold transition duration-300 sm:block">
+              {siteMetadata.headerTitle}
+              <span className="block h-0.5 max-w-0 bg-black transition-all duration-500 group-hover:max-w-[85%] dark:bg-white"></span>
+            </div>
+          ) : (
+            siteMetadata.headerTitle
+          )}
+        </div>
+      </Link>
+      <div className="flex items-center space-x-4 leading-5">
+        <div className="no-scrollbar hidden max-w-40 items-center  overflow-x-auto sm:flex  md:max-w-72 lg:max-w-96">
+          {headerNavLinks
+            .filter((link) => link.href !== '/')
+            .map((link) => (
+              <Link
+                key={link.title}
+                href={link.href}
+                className={clsx(
+                  'mx-1 rounded px-2 py-1 font-medium text-gray-900 dark:text-gray-100 sm:px-3 sm:py-2',
+                  pathname.startsWith(link.href)
+                    ? 'bg-gray-200 dark:bg-primary-400'
+                    : 'hover:bg-gray-200 dark:hover:bg-primary-400'
+                )}
+              >
+                {link.title}
+              </Link>
+            ))}
+        </div>
+        <SearchButton />
+        <ThemeSwitch />
+        <MobileNav />
+      </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ tailwind.config.js
@@ -0,0 +1,89 @@
+// @ts-check
+const { fontFamily } = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
+
+/** @type {import("tailwindcss/types").Config } */
+module.exports = {
+  content: [
+    './node_modules/pliny/**/*.js',
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,tsx}',
+    './components/**/*.{js,ts,tsx}',
+    './layouts/**/*.{js,ts,tsx}',
+    './data/**/*.mdx',
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      lineHeight: {
+        11: '2.75rem',
+        12: '3rem',
+        13: '3.25rem',
+        14: '3.5rem',
+      },
+      fontFamily: {
+        sans: ['var(--font-space-grotesk)', ...fontFamily.sans],
+      },
+      colors: {
+        primary: {
+          50: '#eff4ff',
+          100: '#dce6fd',
+          200: '#c1d3fc',
+          300: '#96b8fa',
+          400: '#7aa2f7',
+          500: '#406cf1',
+          600: '#2b4de5',
+          700: '#2239d3',
+          800: '#2230ab',
+          900: '#212e87',
+          950: '#191f52',
+        },
+        gray: colors.gray,
+        dark: '#1A1B26',
+      },
+      zIndex: {
+        60: '60',
+        70: '70',
+        80: '80',
+      },
+      typography: ({ theme }) => ({
+        DEFAULT: {
+          css: {
+            a: {
+              color: theme('colors.primary.500'),
+              '&:hover': {
+                color: `${theme('colors.primary.600')}`,
+              },
+              code: { color: theme('colors.primary.400') },
+            },
+            'h1,h2': {
+              fontWeight: '700',
+              letterSpacing: theme('letterSpacing.tight'),
+            },
+            h3: {
+              fontWeight: '600',
+            },
+            code: {
+              color: theme('colors.indigo.500'),
+            },
+          },
+        },
+        invert: {
+          css: {
+            a: {
+              color: theme('colors.primary.500'),
+              '&:hover': {
+                color: `${theme('colors.primary.400')}`,
+              },
+              code: { color: theme('colors.primary.400') },
+            },
+            'h1,h2,h3,h4,h5,h6': {
+              color: theme('colors.gray.100'),
+            },
+          },
+        },
+      }),
+    },
+  },
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+}
