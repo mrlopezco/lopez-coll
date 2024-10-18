e717baf87dfe98e717635e25c8a1adf8d83a4f28
ilope <lopez.ignacio@yahoo.de>
2024-10-18 11:59:38 -0600

    Changes in layout

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { useState, useEffect } from 'react'
 import clsx from 'clsx'
 import { usePathname } from 'next/navigation'
 import siteMetadata from '@/data/siteMetadata'
@@ -9,57 +10,85 @@ import Link from './Link'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
 import SearchButton from './SearchButton'
+import SectionContainer from './SectionContainer' // Import the SectionContainer
 
 const Header = () => {
   const pathname = usePathname()
+  const [scrollDirection, setScrollDirection] = useState<'up' | 'down'>('up')
+  const [lastScrollY, setLastScrollY] = useState(0)
 
-  let headerClass = 'bg-white/75 dark:bg-dark/75 upports-backdrop-blur backdrop-blur'
-  if (siteMetadata.stickyNav) {
-    headerClass += ' fixed top-0 z-50 left-0 right-0'
-  }
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.pageYOffset
+
+      if (currentScrollY > lastScrollY) {
+        setScrollDirection('down')
+      } else {
+        setScrollDirection('up')
+      }
+
+      setLastScrollY(currentScrollY)
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [lastScrollY])
+
+  const headerClass = clsx('w-full fixed top-0 z-60 left-0 right-0 transition-top duration-300', {
+    'top-0': scrollDirection === 'up',
+    '-top-20': scrollDirection === 'down', // Adjust the negative top value as needed
+  })
+
+  const innerDivClass =
+    'header-child flex items-center justify-between px-4 py-4 bg-white/75 dark:bg-dark/75 supports-backdrop-blur backdrop-blur sm:px-6 md:rounded-b-2xl xl:shadow-[1px_3px_26px_3px_rgba(0,0,0,0.1)] dark:xl:shadow-[1px_3px_26px_3px_rgba(255,255,255,0.1)]'
 
   return (
     <header className={headerClass}>
-      <div className="mx-auto flex max-w-3xl items-center justify-between px-4 py-4 sm:px-6 xl:max-w-5xl xl:px-0">
-        <Link href="/" aria-label={siteMetadata.headerTitle}>
-          <div className="flex items-center justify-between">
-            <div className="mr-3">
-              <Logo />
-            </div>
-            {typeof siteMetadata.headerTitle === 'string' ? (
-              <div className="group hidden text-xl font-semibold transition duration-300 sm:block">
-                {siteMetadata.headerTitle}
-                <span className="block h-0.5 max-w-0 bg-black transition-all duration-500 group-hover:max-w-[85%] dark:bg-white"></span>
+      <SectionContainer>
+        <div className={innerDivClass} style={{ padding: '1.5rem 8px 1rem' }}>
+          <Link href="/" aria-label={siteMetadata.headerTitle}>
+            <div className="flex items-center justify-between">
+              <div className="mr-3">
+                <Logo />
               </div>
-            ) : (
-              siteMetadata.headerTitle
-            )}
+              {typeof siteMetadata.headerTitle === 'string' ? (
+                <div className="group hidden text-xl font-semibold transition duration-300 sm:block">
+                  {siteMetadata.headerTitle}
+                  <span className="block h-0.5 max-w-0 bg-black transition-all duration-500 group-hover:max-w-[85%] dark:bg-white"></span>
+                </div>
+              ) : (
+                siteMetadata.headerTitle
+              )}
+            </div>
+          </Link>
+          <div className="flex items-center space-x-4 leading-5">
+            <div className="no-scrollbar hidden max-w-40 items-center overflow-x-auto sm:flex md:max-w-72 lg:max-w-96">
+              {headerNavLinks
+                .filter((link) => link.href !== '/')
+                .map((link) => (
+                  <Link
+                    key={link.title}
+                    href={link.href}
+                    className={clsx(
+                      'mx-1 rounded px-2 py-1 font-medium text-gray-900 dark:text-gray-100 sm:px-3 sm:py-2',
+                      pathname.startsWith(link.href)
+                        ? 'bg-gray-200 dark:bg-primary-400'
+                        : 'hover:bg-gray-200 dark:hover:bg-primary-400'
+                    )}
+                  >
+                    {link.title}
+                  </Link>
+                ))}
+            </div>
+            <SearchButton />
+            <ThemeSwitch />
+            <MobileNav />
           </div>
-        </Link>
-        <div className="flex items-center space-x-4 leading-5">
-          <div className="no-scrollbar hidden max-w-40 items-center  overflow-x-auto sm:flex  md:max-w-72 lg:max-w-96">
-            {headerNavLinks
-              .filter((link) => link.href !== '/')
-              .map((link) => (
-                <Link
-                  key={link.title}
-                  href={link.href}
-                  className={clsx(
-                    'mx-1 rounded px-2 py-1 font-medium text-gray-900 dark:text-gray-100 sm:px-3 sm:py-2',
-                    pathname.startsWith(link.href)
-                      ? 'bg-gray-200 dark:bg-primary-400'
-                      : 'hover:bg-gray-200 dark:hover:bg-primary-400'
-                  )}
-                >
-                  {link.title}
-                </Link>
-              ))}
-          </div>
-          <SearchButton />
-          <ThemeSwitch />
-          <MobileNav />
         </div>
-      </div>
+      </SectionContainer>
     </header>
   )
 }
--- components/homepage/Heading.tsx
+++ components/homepage/Heading.tsx
@@ -5,8 +5,8 @@ import { Twemoji } from '@/components/Twemoji'
 const Heading = () => {
   return (
     <h1 className="font-medium text-neutral-900 dark:text-neutral-200">
-      I'm <span>{siteMetadata.author}</span> - <span>Senior Solution Architect</span> for business
-      applications such as{' '}
+      I'm <span>{siteMetadata.author}</span> <br />
+      <span>Senior Solution Architect for Dynamics Applications</span>{' '}
     </h1>
   )
 }
