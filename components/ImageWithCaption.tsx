c0df49de8a3b82d95f29bc8ea1c25b4763df9a47
ilope <lopez.ignacio@yahoo.de>
2024-10-18 20:27:36 -0600

    Style changes and excel article v2

--- components/ImageWithCaption.tsx
+++ components/ImageWithCaption.tsx
@@ -1,12 +1,5 @@
-'use client'
-import { useState, useRef } from 'react'
 import clsx from 'clsx'
-import Lightbox from 'yet-another-react-lightbox'
-import Captions from 'yet-another-react-lightbox/plugins/captions'
-import 'yet-another-react-lightbox/styles.css'
-import 'yet-another-react-lightbox/plugins/captions.css'
 import Image from 'next/image'
-import NextJsImage from './NextJsImage'
 
 const basePath = process.env.BASE_PATH
 
@@ -18,54 +11,20 @@ type Props = {
 }
 
 const ImageWithCaption = ({ src, alt, caption, className }: Props) => {
-  const [open, setOpen] = useState(false)
-  const captionsRef = useRef(null)
+  const fullSrc = `${basePath || ''}${src}`
 
   return (
-    <>
-      <figure className={clsx('text-center', className)}>
-        <button
-          onClick={() => setOpen(true)}
-          style={{ border: 'none', background: 'none', padding: 0, cursor: 'pointer' }}
-          aria-label={`Open lightbox for ${alt}`}
-          onKeyDown={(e) => {
-            if (e.key === 'Enter' || e.key === ' ') {
-              setOpen(true)
-            }
-          }}
-        >
-          <Image
-            src={`${basePath || ''}${src}`}
-            alt={alt}
-            className="mx-auto"
-            width={800} // Adjust these dimensions as necessary
-            height={600} // Adjust these dimensions as necessary
-          />
-        </button>
-        <figcaption>{caption}</figcaption>
-      </figure>
-
-      <Lightbox
-        open={open}
-        close={() => setOpen(false)}
-        slides={[
-          {
-            src: `${basePath || ''}${src}`,
-            width: 800,
-            height: 600,
-            title: alt,
-            description: caption,
-          },
-        ]}
-        render={{
-          slide: NextJsImage,
-          buttonPrev: () => null,
-          buttonNext: () => null,
-        }}
-        plugins={[Captions]}
-        captions={{ ref: captionsRef, descriptionTextAlign: 'start', descriptionMaxLines: 3 }}
+    <figure className={clsx('text-center', className)}>
+      <Image
+        src={fullSrc}
+        alt={alt}
+        className="mx-auto"
+        layout="responsive"
+        width={700}
+        height={475}
       />
-    </>
+      <figcaption>{caption}</figcaption>
+    </figure>
   )
 }
 
--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -7,10 +7,12 @@ import CustomLink from './Link'
 import TableWrapper from './TableWrapper'
 import Twemoji from './Twemoji'
 import ImageWithCaption from './ImageWithCaption'
+import ImageWithLightBox from './ImageWithLightBox'
 
 export const components: MDXComponents = {
   Image,
   ImageWithCaption,
+  ImageWithLightBox,
   TOCInline,
   a: CustomLink,
   pre: Pre,
