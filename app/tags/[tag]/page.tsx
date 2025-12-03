ed6d6e51ee3b9e300f9411221006bb9ada759a80
Ignacio Lopez Coll <joseignaciolopezcoll@gmail.com>
2025-12-03 11:23:16 -0700

    Added post series layout

--- app/tags/[tag]/page.tsx
+++ app/tags/[tag]/page.tsx
@@ -40,7 +40,13 @@ export default async function TagPage(props: { params: Promise<{ tag: string }> 
   // Capitalize first letter and convert space to dash
   const title = tag[0].toUpperCase() + tag.split(' ').join('-').slice(1)
   const filteredPosts = allCoreContent(
-    sortPosts(allBlogs.filter((post) => post.tags && post.tags.map((t) => slug(t)).includes(tag)))
+    sortPosts(
+      allBlogs.filter(
+        (post) =>
+          (post.tags && post.tags.map((t) => slug(t)).includes(tag)) ||
+          (post.postseries && slug(post.postseries) === tag)
+      )
+    )
   )
   if (filteredPosts.length === 0) {
     return notFound()
